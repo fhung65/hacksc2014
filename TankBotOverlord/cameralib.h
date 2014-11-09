32729da1ad1059ea15455500a122c302858241ab
Kevin Balke <kbalke@ucla.edu>
2014-11-08 19:21:34 -0800

    changed to unix-style line endings, changed buffer size when capturing photos

--- TankBotOverlord/cameralib.h
+++ TankBotOverlord/cameralib.h
@@ -84,14 +84,14 @@ void buffer_send();
  *******************************************************************************/
 void VC0706_reset()
 {
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_RESET;
-  tx_vcbuffer[3]=0x00;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_RESET;
+    tx_vcbuffer[3]=0x00;
 
-  tx_counter=4;
+    tx_counter=4;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -107,14 +107,14 @@ void VC0706_reset()
  *******************************************************************************/
 void VC0706_get_version()
 {
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_GEN_VERSION;
-  tx_vcbuffer[3]=0x00;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_GEN_VERSION;
+    tx_vcbuffer[3]=0x00;
 
-  tx_counter=4;
+    tx_counter=4;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -130,14 +130,14 @@ void VC0706_get_version()
  *******************************************************************************/
 void VC0706_tv_out_control(int on)
 {
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_TV_OUT_CTRL;
-  tx_vcbuffer[3]=0x01;
-  tx_vcbuffer[4]=on;
-  tx_counter=5;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_TV_OUT_CTRL;
+    tx_vcbuffer[3]=0x01;
+    tx_vcbuffer[4]=on;
+    tx_counter=5;
 
-  buffer_send();
+    buffer_send();
 }
 
 /*******************************************************************************
@@ -153,33 +153,33 @@ void VC0706_tv_out_control(int on)
  *******************************************************************************/
 void VC0706_osd_add_char(int col, int row, String osd_string)
 {
-  unsigned char col_row;
-  int string_length;
-  int i;
+    unsigned char col_row;
+    int string_length;
+    int i;
 
-  col&=0x0f;
-  row&=0x0f;
-  col_row=(unsigned char)(col<<4 | row);
+    col&=0x0f;
+    row&=0x0f;
+    col_row=(unsigned char)(col<<4 | row);
 
-  string_length=osd_string.length();
-  if (string_length>14)
-    string_length=14;		// max 14 osd characters
+    string_length=osd_string.length();
+    if (string_length>14)
+        string_length=14;		// max 14 osd characters
 
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_OSD_ADD_CHAR;
-  tx_vcbuffer[3]=string_length+2;
-  tx_vcbuffer[4]=string_length;		// character number
-  tx_vcbuffer[5]=col_row;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_OSD_ADD_CHAR;
+    tx_vcbuffer[3]=string_length+2;
+    tx_vcbuffer[4]=string_length;		// character number
+    tx_vcbuffer[5]=col_row;
 
-  for (i=0;i<string_length;i++)
-  {
-    tx_vcbuffer[i+6]=osd_string.charAt(i);
-  }
+    for (i=0; i<string_length; i++)
+    {
+        tx_vcbuffer[i+6]=osd_string.charAt(i);
+    }
 
-  tx_counter=string_length+6;
+    tx_counter=string_length+6;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -199,24 +199,24 @@ void VC0706_osd_add_char(int col, int row, String osd_string)
  *******************************************************************************/
 void VC0706_w_h_downsize(int scale_width, int scale_height)
 {
-  int scale;
+    int scale;
 
-  if (scale_width>=2)	scale_width=2;
-  if (scale_height>scale_width)	scale_height=scale_width;
-  scale=(unsigned char)(scale_height<<2 | scale_width);
+    if (scale_width>=2)	scale_width=2;
+    if (scale_height>scale_width)	scale_height=scale_width;
+    scale=(unsigned char)(scale_height<<2 | scale_width);
 
 
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_DOWNSIZE_SIZE;
-  tx_vcbuffer[3]=0x01;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_DOWNSIZE_SIZE;
+    tx_vcbuffer[3]=0x01;
 
-  tx_vcbuffer[4]=scale;		//bit[1:0] width zooming proportion
-  //bit[3:2] height zooming proportion
+    tx_vcbuffer[4]=scale;		//bit[1:0] width zooming proportion
+    //bit[3:2] height zooming proportion
 
-  tx_counter=5;
+    tx_counter=5;
 
-  buffer_send();
+    buffer_send();
 }
 
 /*******************************************************************************
@@ -231,28 +231,28 @@ void VC0706_w_h_downsize(int scale_width, int scale_height)
 void VC0706_read_frame_buffer(unsigned long buffer_address, unsigned long buffer_length)
 {
 
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_READ_FBUF;
-  tx_vcbuffer[3]=0x0c;
-  tx_vcbuffer[4]=FBUF_CURRENT_FRAME;
-  tx_vcbuffer[5]=0x0a;		// 0x0a=data transfer by MCU mode; 0x0f=data transfer by SPI interface
-  tx_vcbuffer[6]=buffer_address>>24;			//starting address
-  tx_vcbuffer[7]=buffer_address>>16;
-  tx_vcbuffer[8]=buffer_address>>8;
-  tx_vcbuffer[9]=buffer_address&0x0ff;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_READ_FBUF;
+    tx_vcbuffer[3]=0x0c;
+    tx_vcbuffer[4]=FBUF_CURRENT_FRAME;
+    tx_vcbuffer[5]=0x0a;		// 0x0a=data transfer by MCU mode; 0x0f=data transfer by SPI interface
+    tx_vcbuffer[6]=buffer_address>>24;			//starting address
+    tx_vcbuffer[7]=buffer_address>>16;
+    tx_vcbuffer[8]=buffer_address>>8;
+    tx_vcbuffer[9]=buffer_address&0x0ff;
 
-  tx_vcbuffer[10]=buffer_length>>24;		// data length
-  tx_vcbuffer[11]=buffer_length>>16;
-  tx_vcbuffer[12]=buffer_length>>8;
-  tx_vcbuffer[13]=buffer_length&0x0ff;
-  tx_vcbuffer[14]=0x00;		// delay time
-  tx_vcbuffer[15]=0x0a;
+    tx_vcbuffer[10]=buffer_length>>24;		// data length
+    tx_vcbuffer[11]=buffer_length>>16;
+    tx_vcbuffer[12]=buffer_length>>8;
+    tx_vcbuffer[13]=buffer_length&0x0ff;
+    tx_vcbuffer[14]=0x00;		// delay time
+    tx_vcbuffer[15]=0x0a;
 
 
-  tx_counter=16;
+    tx_counter=16;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -269,15 +269,15 @@ void VC0706_read_frame_buffer(unsigned long buffer_address, unsigned long buffer
  *******************************************************************************/
 void VC0706_frame_control(byte frame_control)
 {
-  if(frame_control>3)frame_control=3;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_FBUF_CTRL;
-  tx_vcbuffer[3]=0x01;
-  tx_vcbuffer[4]=frame_control;
-  tx_counter=5;
+    if(frame_control>3)frame_control=3;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_FBUF_CTRL;
+    tx_vcbuffer[3]=0x01;
+    tx_vcbuffer[4]=frame_control;
+    tx_counter=5;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -293,15 +293,15 @@ void VC0706_frame_control(byte frame_control)
  *******************************************************************************/
 void VC0706_motion_detection(int control_flag)
 {
-  if(control_flag>1)control_flag=1;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_COMM_MOTION_CTRL;
-  tx_vcbuffer[3]=0x01;
-  tx_vcbuffer[4]=control_flag;
-  tx_counter=5;
+    if(control_flag>1)control_flag=1;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_COMM_MOTION_CTRL;
+    tx_vcbuffer[3]=0x01;
+    tx_vcbuffer[4]=control_flag;
+    tx_counter=5;
 
-  buffer_send();
+    buffer_send();
 }
 
 /*******************************************************************************
@@ -316,17 +316,17 @@ void VC0706_motion_detection(int control_flag)
  *******************************************************************************/
 void VC0706_motion_control(int control_flag)
 {
-  if(control_flag>1)control_flag=1;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_MOTION_CTRL;
-  tx_vcbuffer[3]=0x03;
-  tx_vcbuffer[4]=0x00;			//motion control attribute
-  tx_vcbuffer[5]=0x01;			//mcu uart control
-  tx_vcbuffer[6]=control_flag;
-  tx_counter=7;
+    if(control_flag>1)control_flag=1;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_MOTION_CTRL;
+    tx_vcbuffer[3]=0x03;
+    tx_vcbuffer[4]=0x00;			//motion control attribute
+    tx_vcbuffer[5]=0x01;			//mcu uart control
+    tx_vcbuffer[6]=control_flag;
+    tx_counter=7;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -347,15 +347,15 @@ void VC0706_motion_control(int control_flag)
  *******************************************************************************/
 void VC0706_get_framebuffer_length(byte fbuf_type)
 {
-  if(fbuf_type>1)fbuf_type=1;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_GET_FBUF_LEN;
-  tx_vcbuffer[3]=0x01;
-  tx_vcbuffer[4]=fbuf_type;
-  tx_counter=5;
+    if(fbuf_type>1)fbuf_type=1;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_GET_FBUF_LEN;
+    tx_vcbuffer[3]=0x01;
+    tx_vcbuffer[4]=fbuf_type;
+    tx_counter=5;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -372,16 +372,16 @@ void VC0706_get_framebuffer_length(byte fbuf_type)
  *******************************************************************************/
 void VC0706_uart_power_save(byte power_save_on)
 {
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_POWER_SAVE_CTRL;
-  tx_vcbuffer[3]=0x03;
-  tx_vcbuffer[4]=00;			//power save control mode
-  tx_vcbuffer[5]=01;			// control by UART
-  tx_vcbuffer[6]=power_save_on;			//start power save
-  tx_counter=7;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_POWER_SAVE_CTRL;
+    tx_vcbuffer[3]=0x03;
+    tx_vcbuffer[4]=00;			//power save control mode
+    tx_vcbuffer[5]=01;			// control by UART
+    tx_vcbuffer[6]=power_save_on;			//start power save
+    tx_counter=7;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -398,16 +398,16 @@ void VC0706_uart_power_save(byte power_save_on)
  *******************************************************************************/
 void VC0706_uart_color_control(byte show_mode)
 {
-  if(show_mode>2) show_mode=2;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_COLOR_CTRL;
-  tx_vcbuffer[3]=0x02;
-  tx_vcbuffer[4]=01;		//control by UART
-  tx_vcbuffer[5]=show_mode;		// automatically step black-white and colour
-  tx_counter=6;
+    if(show_mode>2) show_mode=2;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_COLOR_CTRL;
+    tx_vcbuffer[3]=0x02;
+    tx_vcbuffer[4]=01;		//control by UART
+    tx_vcbuffer[5]=show_mode;		// automatically step black-white and colour
+    tx_counter=6;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -416,7 +416,7 @@ void VC0706_uart_color_control(byte show_mode)
  * Function Name  : VC0706_compression_ratio
  * Description	  : stop current frame for reading
  *
- * Input		  : ration		>13(minimum)
+ * Input		  : ratio		>13(minimum)
  *						<63(max)
  *
  * Output		  : None
@@ -424,22 +424,22 @@ void VC0706_uart_color_control(byte show_mode)
  *******************************************************************************/
 void VC0706_compression_ratio(int ratio)
 {
-  if(ratio>63)ratio=63;
-  if(ratio<13)ratio=13;
-  int vc_comp_ratio=(ratio-13)*4+53;
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_WRITE_DATA;
-  tx_vcbuffer[3]=0x05;
-  tx_vcbuffer[4]=01;		//chip register
-  tx_vcbuffer[5]=0x01;	//data num ready to write
-  tx_vcbuffer[6]=0x12;	//register address
-  tx_vcbuffer[7]=0x04;
-  tx_vcbuffer[8]=vc_comp_ratio; //data
+    if(ratio>63)ratio=63;
+    if(ratio<13)ratio=13;
+    int vc_comp_ratio=(ratio-13)*4+53;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_WRITE_DATA;
+    tx_vcbuffer[3]=0x05;
+    tx_vcbuffer[4]=01;		//chip register
+    tx_vcbuffer[5]=0x01;	//data num ready to write
+    tx_vcbuffer[6]=0x12;	//register address
+    tx_vcbuffer[7]=0x04;
+    tx_vcbuffer[8]=vc_comp_ratio; //data
 
-  tx_counter=9;
+    tx_counter=9;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -455,24 +455,24 @@ void VC0706_compression_ratio(int ratio)
  *******************************************************************************/
 void VC0706_motion_windows_setting(unsigned int register_address, unsigned long data)
 {
-  tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
-  tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
-  tx_vcbuffer[2]=VC0706_COMMAND_WRITE_DATA;
-  tx_vcbuffer[3]=0x08;
-  tx_vcbuffer[4]=01;		//chip register
-  tx_vcbuffer[5]=0x04;	//data num ready to write
-  tx_vcbuffer[6]=register_address>>8;	//register address
-  tx_vcbuffer[7]=register_address&0x0ff;
-  ;
+    tx_vcbuffer[0]=VC0706_PROTOCOL_SIGN;
+    tx_vcbuffer[1]=VC0706_SERIAL_NUMBER;
+    tx_vcbuffer[2]=VC0706_COMMAND_WRITE_DATA;
+    tx_vcbuffer[3]=0x08;
+    tx_vcbuffer[4]=01;		//chip register
+    tx_vcbuffer[5]=0x04;	//data num ready to write
+    tx_vcbuffer[6]=register_address>>8;	//register address
+    tx_vcbuffer[7]=register_address&0x0ff;
+    ;
 
-  tx_vcbuffer[8]=data>>24;		// data ready to write
-  tx_vcbuffer[9]=data>>16;
-  tx_vcbuffer[10]=data>>8;
-  tx_vcbuffer[11]=data&0x0ff;
+    tx_vcbuffer[8]=data>>24;		// data ready to write
+    tx_vcbuffer[9]=data>>16;
+    tx_vcbuffer[10]=data>>8;
+    tx_vcbuffer[11]=data&0x0ff;
 
-  tx_counter=12;
+    tx_counter=12;
 
-  buffer_send();
+    buffer_send();
 }
 
 
@@ -489,15 +489,15 @@ void VC0706_motion_windows_setting(unsigned int register_address, unsigned long 
  *******************************************************************************/
 void debug_send()
 {
-  int i=0;
+    int i=0;
 
-  for (i=0;i<tx_counter;i++)
-  {
-    Serial.print(tx_vcbuffer[i], HEX);
-    Serial.print(", ");
-  }
+    for (i=0; i<tx_counter; i++)
+    {
+        Serial.print(tx_vcbuffer[i], HEX);
+        Serial.print(", ");
+    }
 
-  Serial.println("");
+    Serial.println("");
 }
 
 
@@ -513,12 +513,12 @@ void debug_send()
  *******************************************************************************/
 void buffer_send()
 {
-  int i=0;
+    int i=0;
 
-  for (i=0;i<tx_counter;i++)
-    Serial.write(tx_vcbuffer[i]);
+    for (i=0; i<tx_counter; i++)
+        Serial.write(tx_vcbuffer[i]);
 
-  tx_ready=true;
+    tx_ready=true;
 }
 
 
@@ -534,25 +534,25 @@ void buffer_send()
  *******************************************************************************/
 void buffer_read()
 {
-  bool validity=true;
+    bool validity=true;
 
-  if (rx_ready)			// if something unread in buffer, just quit
-    return;
+    if (rx_ready)			// if something unread in buffer, just quit
+        return;
 
-  rx_counter=0;
-  VC0706_rx_buffer[0]=0;
-  while (Serial.available() > 0)
-  {
-    VC0706_rx_buffer[rx_counter++]= Serial.read();
-    //delay(1);
-  }
+    rx_counter=0;
+    VC0706_rx_buffer[0]=0;
+    while (Serial.available() > 0)
+    {
+        VC0706_rx_buffer[rx_counter++]= Serial.read();
+        //delay(1);
+    }
 
-  if (VC0706_rx_buffer[0]!=0x76)
-    validity=false;
-  if (VC0706_rx_buffer[1]!=VC0706_SERIAL_NUMBER)
-    validity=false;
+    if (VC0706_rx_buffer[0]!=0x76)
+        validity=false;
+    if (VC0706_rx_buffer[1]!=VC0706_SERIAL_NUMBER)
+        validity=false;
 
-  if (validity) rx_ready=true;
+    if (validity) rx_ready=true;
 
 
 }
@@ -566,84 +566,69 @@ void buffer_read()
  * Output		  : None
  * Return		  : None
  *******************************************************************************/
-void capture_photo_to_buffer(uint8_t * dataBuf, uint32_t dataBufSize, void(callback*)(uint8_t*)){
+void capture_photo_to_buffer(uint8_t * dataBuf, uint32_t dataBufSize, void(callback*)(uint8_t*))
+{
+    // Set compression ratio
+    VC0706_compression_ratio(63);
+    delay(100);
 
-  // Check to see if the file exists:
-  // if exists,delete the file:
-  if(sd.exists("temp.jpg")) sd.remove("temp.jpg");
+    VC0706_frame_control(3);
+    delay(10);
 
-  // open a new empty file for write at end like the Native SD library
-  if (!myFile.open("temp.jpg", O_RDWR | O_CREAT | O_AT_END)) {
-    sd.errorHalt("opening temp.jpg for write failed");
-  }
+    VC0706_frame_control(0);
+    delay(10);
 
-  // close the file:
-  myFile.close();
+    // Update state machine
+    rx_ready=false;
+    rx_counter=0;
 
-  VC0706_compression_ratio(63);
-  delay(100);
+    //get frame buffer length
+    VC0706_get_framebuffer_length(0);
+    delay(10);
+    buffer_read();
 
-  VC0706_frame_control(3);
-  delay(10);
+    // store frame buffer length for coming reading
+    frame_length=(VC0706_rx_buffer[5]<<8)+VC0706_rx_buffer[6];
+    frame_length<<=16;
+    frame_length+=(0x0ff00&(VC0706_rx_buffer[7]<<8))+VC0706_rx_buffer[8];
 
-  VC0706_frame_control(0);
-  delay(10);
-  rx_ready=false;
-  rx_counter=0;
+    vc_frame_address =dataBufSize;
 
-  Serial.end();			// clear all rx buffer
-  delay(5);
+    myFile.open("temp.jpg", O_RDWR);
+    while(vc_frame_address<frame_length)
+    {
+        VC0706_read_frame_buffer(vc_frame_address-dataBufSize, dataBufSize);
+        delay(9);
 
-  Serial.begin(115200);
+        //get the data with length=READ_DATA_BLOCK_NObytes
+        rx_ready=false;
+        rx_counter=0;
+        buffer_read();
 
-  //get frame buffer length
-  VC0706_get_framebuffer_length(0);
-  delay(10);
-  buffer_read();
+        // write data to temp.jpg
+        memcpy(VC0706_rx_buffer+5, dataBuf, dataBufSize);
 
-  //while(1){};
+        //read next READ_DATA_BLOCK_NO bytes from frame buffer
+        vc_frame_address+=dataBufSize;
 
-  // store frame buffer length for coming reading
-  frame_length=(VC0706_rx_buffer[5]<<8)+VC0706_rx_buffer[6];
-  frame_length=frame_length<<16;
-  frame_length=frame_length+(0x0ff00&(VC0706_rx_buffer[7]<<8))+VC0706_rx_buffer[8];
+    }
 
-  vc_frame_address =READ_DATA_BLOCK_NO;
+    // get the last data
+    vc_frame_address-=dataBufSize;
 
-  myFile.open("temp.jpg", O_RDWR);
-  while(vc_frame_address<frame_length){
-    VC0706_read_frame_buffer(vc_frame_address-READ_DATA_BLOCK_NO, READ_DATA_BLOCK_NO);
+    last_data_length=frame_length-vc_frame_address;
+
+
+    VC0706_read_frame_buffer(vc_frame_address,last_data_length);
     delay(9);
-
-    //get the data with length=READ_DATA_BLOCK_NObytes
+    //get the data
     rx_ready=false;
     rx_counter=0;
     buffer_read();
 
-    // write data to temp.jpg
-    myFile.write(VC0706_rx_buffer+5,READ_DATA_BLOCK_NO);
+    memcpy(VC0706_rx_buffer+5, dataBuf, last_data_length);
 
-    //read next READ_DATA_BLOCK_NO bytes from frame buffer
-    vc_frame_address=vc_frame_address+READ_DATA_BLOCK_NO;
-
-  }
-
-  // get the last data
-  vc_frame_address=vc_frame_address-READ_DATA_BLOCK_NO;
-
-  last_data_length=frame_length-vc_frame_address;
-
-
-  VC0706_read_frame_buffer(vc_frame_address,last_data_length);
-  delay(9);
-  //get the data
-  rx_ready=false;
-  rx_counter=0;
-  buffer_read();
-
-  myFile.write(VC0706_rx_buffer+5,last_data_length);
-
-  myFile.close();
+    myFile.close();
 
 }
 
